4912891f030367a6517d02a9d9bfdf6a62bbeefa
zetRC <zetrosas555@gmail.com>
2021-04-25 18:52:55 -0500

    -added leaflet library -layout changes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-function initMap() {
-    //  location 
-    const uluru = { lat: 32.80881103833472, lng: -96.81399569072752 };
-    //  map
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    //marker
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+$(document).ready(function(){
+
+  function initMap() {
+
+    var mymap = L.map('mapid').setView([29.79, -98.42], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiaG9odXNlcjEyMyIsImEiOiJja25ldHM1Y2YwMHl0MnhwYTFjcmhrMjd1In0.oWaWYfJfKI3HsQrmx-huDA'
+    }).addTo(mymap);
+
+    var marker = L.marker([29.79, -98.42]).addTo(mymap);
+    marker.bindPopup("<h5>Property</h5>").openPopup();
+  }
+
+  function selectGaleryImages() {
+    
+    $('.galery img').click(function(){
+      window.open($(this)[0].src, '_blank')
     });
   }
-  
-  
-  $(document).ready(function(){
-    $('.galery img').click(function(){
-  
-      window.open($(this)[0].src, '_blank')
-  
-    });
-  
-  });
+
+  initMap()
+  selectGaleryImages()
+
+});
